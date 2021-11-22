441eb7142ec431718d44a3459b8dc034b684b750
Pramuda Liyanage <pramudatharika@gmail.com>
2021-11-22 22:48:41 +0530

    first commit

---
+++ src/main/kotlin/com/capitalmaharaja/stitchline/StitchLineApplication.kt
@@ -0,0 +1,11 @@
+package com.capitalmaharaja.stitchline
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StitchLineApplication
+
+fun main(args: Array<String>) {
+    runApplication<StitchLineApplication>(*args)
+}
---
+++ src/test/kotlin/com/capitalmaharaja/stitchline/StitchLineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.capitalmaharaja.stitchline
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StitchLineApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
